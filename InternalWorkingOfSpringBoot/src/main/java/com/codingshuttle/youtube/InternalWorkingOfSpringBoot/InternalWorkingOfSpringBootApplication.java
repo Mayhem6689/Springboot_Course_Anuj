63b57ff35d2a3b6c96cc9c94012e53b01002390b
Mayhem6689 <harshitgupt333@gmail.com>
2025-07-29 02:10:10 +0530

    added files for #2 internal-working

---
+++ InternalWorkingOfSpringBoot/src/main/java/com/codingshuttle/youtube/InternalWorkingOfSpringBoot/InternalWorkingOfSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.youtube.InternalWorkingOfSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalWorkingOfSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalWorkingOfSpringBootApplication.class, args);
+	}
+
+}
---
+++ InternalWorkingOfSpringBoot/src/test/java/com/codingshuttle/youtube/InternalWorkingOfSpringBoot/InternalWorkingOfSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.youtube.InternalWorkingOfSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalWorkingOfSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
